8a1551198e59cc5c86b44b7e0a07bf34fd7c4ed2
Markus Scheidgen <markus.scheidgen@gmail.com>
2017-06-19 16:34:09 +0200

    Simplefied the project even more.

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # D3ng.Example
 
+This is a very simple example project or project skeleton for [d3ng](https://github.com/markus1978/d3ng).
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
 ## Development server
+
+Run `npm update` to retrieve the necessary dependencies.
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, Injectable} from '@angular/core';
+import {Http, Response} from "@angular/http";
+import 'rxjs/Rx';
+import * as d3 from "d3";
+import {D3ngGroupContext} from "d3ng/d3ng-groups.component";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <d3ng-groups [context]="context">
+      <d3ng-scatter-plot [data]="data"
+                         x="sepal length" y="sepal width"
+                         [dimensions]="dimensions">
+      </d3ng-scatter-plot>  
+    </d3ng-groups>
+
+    <d3ng-groups [context]="context">
+      <d3ng-scatter-plot [data]="data"
+                         x="petal length" y="petal width"
+                         [dimensions]="dimensions">
+      </d3ng-scatter-plot>
+    </d3ng-groups>
+  `,
+  styles: ['d3ng-scatter-plot { width: 100%; height: 300px; }']
 })
+
+@Injectable()
 export class AppComponent {
-  title = 'app works!';
+  context = new D3ngGroupContext();
+  data: any[] = null;
+  dimensions = ['sepal length',	'sepal width', 'petal length', 'petal width'];
+
+  constructor(http: Http) {
+    http.get("http://mbostock.github.io/d3/talk/20111116/iris.csv")
+      .map((res: Response) => res)
+      .subscribe(res => {
+        this.data = d3.csv.parse(res.text()).map(d => {
+          // convert data, since d3 creates strings and not numbers
+          const result = {};
+          this.dimensions.forEach(dim => result[dim] = parseFloat(d[dim]));
+          return result;
+        });
+      });
+  }
 }
